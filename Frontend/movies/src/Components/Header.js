7dae53dcc7cfdb1facd4c8dbddfcdbf8dc646376
JehanRodrigo <138384440+JehanRodrigo@users.noreply.github.com>
2024-02-24 23:06:46 +0530

    3rd Commit (Created Header n Navbar)
    (Created Header n Navbar) initially.......

--- Frontend/movies/src/Components/Header.js
+++ Frontend/movies/src/Components/Header.js
@@ -1,7 +1,35 @@
 import React from 'react'
-
+import { AppBar, Autocomplete, Toolbar, Box, TextField, Tabs, Tab } from "@mui/material";
+import MovieIcon from '@mui/icons-material/Movie';
+const dummyArray = ["Flash","Batman","Superman"];
 const Header = () => {
-  return <div>Header</div>;
-}
+  return (
+  <AppBar>
+    <Toolbar>
+      <Box width={'20%'}>
+          <MovieIcon/>
+      </Box>
+      
+      <Box width={"30%"} margin={"auto"}>
+      <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={dummyArray.map((option) => option)}
+        renderInput={(params) => 
+        <TextField variant='standard'{...params} label="Search Movies" />}
+      />
+      </Box>
+
+      <Box display={'flex'}>
+          <Tabs >
+            <Tab label="Movies"/>
+            <Tab label="admin"/>
+            <Tab label="Auth"/>
+          </Tabs>
+      </Box>
+    </Toolbar>
+  </AppBar>
+  );
+};
 
 export default Header
