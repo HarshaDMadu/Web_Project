9fdd3092c1317ceed3a7fe48bf11ecee311217ed
JehanRodrigo <138384440+JehanRodrigo@users.noreply.github.com>
2024-02-25 18:01:06 +0530

    4th commit (Header Completed)

--- Frontend/movies/src/Components/Header.js
+++ Frontend/movies/src/Components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { AppBar, Autocomplete, Toolbar, Box, TextField, Tabs, Tab } from "@mui/material";
+import { 
+  AppBar, 
+  Autocomplete, 
+  Toolbar, 
+  Box, 
+  TextField, 
+  Tabs, 
+  Tab 
+} from "@mui/material";
 import MovieIcon from '@mui/icons-material/Movie';
+import { useState } from 'react';
 const dummyArray = ["Flash","Batman","Superman"];
 const Header = () => {
+    const [Value, setValue] = useState(0);
   return (
-  <AppBar>
+  <AppBar sx={{bgcolor: "#2b2d42"}}>
     <Toolbar>
       <Box width={'20%'}>
           <MovieIcon/>
@@ -16,12 +26,20 @@ const Header = () => {
         freeSolo
         options={dummyArray.map((option) => option)}
         renderInput={(params) => 
-        <TextField variant='standard'{...params} label="Search Movies" />}
+          <TextField sx={{input: {color:'white'}}}
+          variant='standard'{...params} placeholder="Search Movies" 
+          />
+        }
       />
       </Box>
 
       <Box display={'flex'}>
-          <Tabs >
+          <Tabs 
+          textColor="inherit" 
+          indicatorColor="secondary" 
+          value={Value} 
+          onChange={(e,val)=>setValue(val)}
+          >
             <Tab label="Movies"/>
             <Tab label="admin"/>
             <Tab label="Auth"/>
