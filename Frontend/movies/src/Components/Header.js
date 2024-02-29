5b55aec7dea7cfdcb65ecc65b86e7d22c9dd6eea
JehanRodrigo <138384440+JehanRodrigo@users.noreply.github.com>
2024-02-29 19:43:50 +0530

    5th Commit Update Header.js
    +used movies (Movies Not defined Error)
    +Omitted the dummy array of search bar drop down list. (causes errors in the search bar)

--- Frontend/movies/src/Components/Header.js
+++ Frontend/movies/src/Components/Header.js
@@ -12,25 +12,26 @@ import MovieIcon from '@mui/icons-material/Movie';
 import { useState } from 'react';
 import { getAllMovies } from '../api-helpers/API-helpers';
 const dummyArray = ["Flash","Batman","Superman"];
+
 const Header = () => {
     const [Value, setValue] = useState(0);
+    /*const [Movies, setMovies] = useState([]);
     useEffect (() => {
       getAllMovies()
-      .then((data)=>console.log(data))
+      .then((data)=>setMovies(data.movies))
       .catch((err)=>console.log(err));
-    },[])
+    },[])*/  //Movies Not Defined error !!!!!
   return (
   <AppBar sx={{bgcolor: "#2b2d42"}}>
     <Toolbar>
       <Box width={'20%'}>
           <MovieIcon/>
       </Box>
-      
       <Box width={"30%"} margin={"auto"}>
       <Autocomplete
         id="free-solo-demo"
         freeSolo
-        options={dummyArray.map((option) => option)}
+    //    options={movies && movies.map((option) => option.title)}
         renderInput={(params) => 
           <TextField sx={{input: {color:'white'}}}
           variant='standard'{...params} placeholder="Search Movies" 
