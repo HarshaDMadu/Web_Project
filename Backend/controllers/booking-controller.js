912aedf94ceaac3d4e287e40be5c2d9de35db3a2
HarshaDMadu <harshadhananjaya14@gmail.com>
2024-03-03 01:00:10 +0530

    Complete Backend

--- Backend/controllers/booking-controller.js
+++ Backend/controllers/booking-controller.js
@@ -54,3 +54,44 @@ export const newBooking = async(req,res,next)=>{
 
     return res.status(201).json({newBooking});
 }
+
+export const getBookingById = async(req,res,next) => {
+    const id = req.params.id;
+    let booking;
+
+    try {
+        booking = await Bookings.findById(id);
+    } catch (err) {
+        return console.log(err)
+    }
+    if(!booking){
+        return res.status(500).json({message:"Unexpected Error"})
+    }
+    return res.status(200).json({ booking })
+}
+
+export const deleteBooking = async(req,res,next) => {
+    const id = req.params.id;
+    let booking;
+
+    try {
+        booking = await Bookings.findByIdAndDelete(id).populate("movie user");
+        console.log(booking);
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await booking.user.booking.pull(booking);
+        await booking.movie.bookings.pull(booking);
+        await booking.movie.save({session});
+        await booking.user.save({session});
+        session.commitTransaction();
+
+    } catch (err) {
+        return console.log(err);
+        
+    }
+    if(!booking){
+        return res.status(500).json({message: "Unable to Delete"});
+    }
+
+    return res.status(200).json({message: "Delete Successfully"});
+}
--- Backend/routes/user-rotes.js
+++ Backend/routes/user-rotes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import getAllUsers, { deleteUser, login, signup, updateUser } from "../controllers/user-controller.js";
+import getAllUsers, { deleteUser, getBookingsOfUser, login, signup, updateUser } from "../controllers/user-controller.js";
 
 const userRouter = express.Router();
 userRouter.get("/", getAllUsers);
@@ -7,5 +7,6 @@ userRouter.post("/signup", signup);
 userRouter.put("/:id",updateUser);
 userRouter.delete("/:id", deleteUser);
 userRouter.post("/login", login);
+userRouter.get("/bookings/:id", getBookingsOfUser);
 
 export default userRouter;
