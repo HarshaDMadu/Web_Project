37cf20b78beeb82751b3af3508c3771877940fe7
HarshaDMadu <harshadhananjaya14@gmail.com>
2024-03-12 01:15:05 +0530

    Search bar

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,25 @@
 import express from "express";
+import cors from "cors";
 
 import mongoose from "mongoose";
 import userRouter from "./routes/user-rotes.js";
 import adminRouter from "./routes/admin-routes.js";
 import movieRouter from "./routes/movie-routes.js";
 import bookingsRouter from "./routes/booking-routes.js";
+
+
+
 const app = express();
 
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 app.use("/user",userRouter);
 app.use("/admin",adminRouter);
 app.use("/movie",movieRouter);
 app.use("/booking",bookingsRouter);
+
 
 mongoose
 .connect(`mongodb+srv://admin:1234@cluster0.rgfbnve.mongodb.net/?retryWrites=true&w=majority`
